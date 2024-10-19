5239d42de661618cb210790c4a6d4d4fa0303350
Mandragora <dan.investor.1996@gmail.com>
2024-10-19 20:48:36 +0200

    Update mandrainstaller.sh

--- story/mandrainstaller.sh
+++ story/mandrainstaller.sh
@@ -45,39 +45,38 @@ if [ "$OPTION" == "1" ]; then
 			story init --network iliad --moniker $MONIKER
 			
 			echo -e "$GREEN Creating service files for story and geth.$NORMAL"
-
-   			sudo tee /etc/systemd/system/story.service <<- EOF > /dev/null
-				[Unit]
-				Description=Story Consensus Client
-				After=network.target
-	
-				[Service]
-				User=root
-				ExecStart=/usr/local/bin/story run
-				Restart=on-failure
-				RestartSec=3
-				LimitNOFILE=65535
-	
-				[Install]
-				WantedBy=multi-user.target
-		         EOF
-	   
-			 sudo tee /etc/systemd/system/story-geth.service <<- EOF > /dev/null
-				 [Unit]
-				 Description=Story Geth Client
-				 After=network.target
+sudo tee /etc/systemd/system/story-geth.service <<EOF
+   [Unit]
+Description=Story Geth Client
+ After=network.target
 		
-				[Service]
-				User=root
-				ExecStart=/usr/local/bin/story-geth --iliad --syncmode full
-				Restart=on-failure
-				RestartSec=3
-				LimitNOFILE=65535
+[Service]
+User=root
+ExecStart=/usr/local/bin/story-geth --iliad --syncmode full
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
 	
-				[Install]
-				WantedBy=multi-user.target
-			EOF
-   
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo tee /etc/systemd/system/story.service <<EOF
+  [Unit]
+Description=Story Consensus Client
+After=network.target
+	
+[Service]
+User=root
+ExecStart=/usr/local/bin/story run
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+	
+[Install]
+WantedBy=multi-user.target
+
+EOF
 			echo -e "$GREEN Enabling service files.$NORMAL"
 			sudo systemctl daemon-reload
 			sudo systemctl enable story-geth
